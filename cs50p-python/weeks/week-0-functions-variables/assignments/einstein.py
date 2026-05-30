5d7cbf7aeb389631b20a581b234fcb51a627d6f6
Jodrohan <rohansomriya12345@gmail.com>
2026-05-30 17:14:34 +0530

    Refactor einstein.py to use main function and fix check50 formatting

--- cs50p-python/weeks/week-0-functions-variables/assignments/einstein.py
+++ cs50p-python/weeks/week-0-functions-variables/assignments/einstein.py
@@ -1,11 +1,11 @@
 #Write a Python program that asks the user for mass as an integer and calculates energy using Einstein’s formula:
-
 #E = m × c²
-
 #where c = 300000000
 
+def main():
+    m = int(input("m: "))
+    c = 300000000
+    E = m*c*c
+    print(E)
 
-mass = int(input("Enter mass: "))
-c = 300000000
-E = mass*c*c
-print(E)
+main()
