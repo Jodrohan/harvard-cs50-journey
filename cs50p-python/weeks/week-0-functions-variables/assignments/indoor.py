c18825e998c892d9e92acf80c2172fa026e96611
Jodrohan <rohansomriya12345@gmail.com>
2026-05-30 17:19:51 +0530

    Fix check50 formatting in indoor.py

--- cs50p-python/weeks/week-0-functions-variables/assignments/indoor.py
+++ cs50p-python/weeks/week-0-functions-variables/assignments/indoor.py
@@ -1,3 +1,6 @@
 #Write a Python program that takes user input and prints the same text in lowercase.
-a = input("Enter a string: ")
-print(a.lower())
+def main():
+    a = input()
+    print(a.lower())
+    
+main()
