cd1dc68e036c0bac47d378df3d7322f2a6c7b7a1
Jodrohan <rohansomriya12345@gmail.com>
2026-05-30 16:49:20 +0530

    Complete tip calculator assignment

---
+++ cs50p-python/weeks/week-0-functions-variables/assignments/tip.py
@@ -0,0 +1,28 @@
+# Write a Python program that takes a meal cost and a tip percentage as input, calculates the tip amount, and prints the final total to leave.
+def main():
+
+    dollars = dollars_to_float(input("Enter the cost of the meal: "))
+   
+    tip = percent_to_float(input("Enter the percentage of amount you wanna give tip: "))
+    
+    tip = tip * dollars
+
+    print(f"Leave ${tip:.2f}")
+
+
+
+def dollars_to_float(d):
+   
+    d = d.replace("$","")
+    d = float(d)
+    return(d)
+
+
+def percent_to_float(p):
+   
+    p = p.replace("%","")
+    p = float(p)
+    p = p/100
+    return(p)
+
+main()
