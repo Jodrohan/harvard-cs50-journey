929d22cf5cb33f55d6b992cbe8838b7d24a200f8
Jodrohan <rohansomriya12345@gmail.com>
2026-05-30 17:31:46 +0530

    Refactor faces.py to use structural functions

--- cs50p-python/weeks/week-0-functions-variables/assignments/faces.py
+++ cs50p-python/weeks/week-0-functions-variables/assignments/faces.py
@@ -1,8 +1,10 @@
 #Write a Python program that replaces :) with 🙂 and :( with 🙁 in user input.
 
-emoji = input("Enter the string: ")
-
-
-emoji=emoji.replace(":)","🙂")
-emoji=emoji.replace(":(","🙁")
-print(emoji)
+def main():
+    emoji = convert(input())
+    print(emoji)
+def convert(c):
+    c = c.replace(":)","🙂")
+    c = c.replace(":(","🙁")
+    return c
+main()
