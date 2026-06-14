07e7701a3ce5a9a6e402a255dce030fc48ea5c5f
Jodrohan <rohansomriya12345@gmail.com>
2026-06-15 05:01:59 +0530

    feat: build python experiments using requests, sys, and custom modules

---
+++ cs50p-python/weeks/week-4-libraries/experiments/generate.py
@@ -0,0 +1,15 @@
+import random
+'''
+coin = random.choice(["heads","tails"])
+print(coin)
+'''
+'''
+number = random.randint(1,10)
+print(number)
+'''
+'''
+cards = ["jack", "king", "queen"]
+shuffle = random.shuffle(cards)
+for card in cards:
+    print(card)
+'''
---
+++ cs50p-python/weeks/week-4-libraries/experiments/name.py
@@ -0,0 +1,12 @@
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("too less arguments")
+for arg in sys.argv[1:3]:
+    print("hello, my name is", arg)
+
+
+
+
+    
+
