25e731405378f37d1def4313c3457641561daf2e
Jodrohan <rohansomriya12345@gmail.com>
2026-06-14 17:53:38 +0530

    feat: complete taqueria.py with robust EOF handling and clean logic

---
+++ cs50p-python/weeks/week-3-exceptions/assignments/taqueria.py
@@ -0,0 +1,50 @@
+'''
+Felipe’s Taqueria (taqueria.py)
+
+Implement a program that enables a user to place an order, prompting them 
+for items, one per line, until the user inputs control-d (which is a 
+common way of ending one's input to a program). 
+
+Requirements:
+- After each inputted item, display the total cost of all items inputted 
+  so far, prefixed with a dollar sign ($) and formatted to two decimal places.
+- Treat the user’s input case-insensitively (e.g., "Taco" and "taco" are the same).
+- If an item is not on the menu, ignore that input.
+- You must handle the EOFError when the user presses control-d.
+
+Menu:
+Baja Taco: 4.00
+Burrito: 7.50
+Bowl: 8.50
+Nachos: 11.00
+Quesadilla: 8.50
+Super Burrito: 8.50
+Super Quesadilla: 9.50
+Taco: 3.00
+Tortilla Salad: 8.00
+'''
+def main():
+  menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00,
+  }
+  total = 0
+  while True:
+    try:
+      order = input("What's order?").title().strip()
+    except EOFError:
+      pass
+      break
+    else:
+      if order in menu:
+        total += menu[order]
+        print(f"Total Bill: ${total:.2f}")
+if __name__ == "__main__":
+  main()
