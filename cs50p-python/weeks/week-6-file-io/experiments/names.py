71b11199aeaf5b2b79f7ca67dbbcd79920db32ab
Jodrohan <rohansomriya12345@gmail.com>
2026-06-29 10:40:10 +0530

    feat: implement text and CSV file reading and writing

---
+++ cs50p-python/weeks/week-6-file-io/experiments/names.py
@@ -0,0 +1,8 @@
+name = input("What's your name? ")
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
+
+with open("names.txt", "r") as file:
+    for line in sorted(file):
+        print(f"hello, {line.rstrip()}")
