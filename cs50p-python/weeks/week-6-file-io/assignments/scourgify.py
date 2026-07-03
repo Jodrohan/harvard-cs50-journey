3038b8a9e1acd9149f081c2f8029fcd25f066eec
Jodrohan <rohansomriya12345@gmail.com>
2026-07-03 11:51:41 +0530

    docs: add docstring for scourgify assignment

---
+++ cs50p-python/weeks/week-6-file-io/assignments/scourgify.py
@@ -0,0 +1,10 @@
+"""
+CS50P Week 6 - File I/O
+Assignment 3: Scourgify
+
+Objective: 
+- Read an existing CSV file containing student names formatted as "Last, First".
+- Split the names into separate "First" and "Last" columns.
+- Write the cleaned data to a new CSV file.
+- Handle command-line arguments and file-not-found exceptions.
+"""
