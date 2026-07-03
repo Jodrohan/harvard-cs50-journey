3c00e686eaaaa50a288ba79eaa27403e6097de03
Jodrohan <rohansomriya12345@gmail.com>
2026-07-03 09:33:44 +0530

    feat: implement CSV parsing and ASCII table formatting

--- cs50p-python/weeks/week-6-file-io/assignments/pizza.py
+++ cs50p-python/weeks/week-6-file-io/assignments/pizza.py
@@ -17,29 +17,29 @@ Table Formatting Logic:
 - The very first row of the CSV file must be used as the table's headers.
 - The table must be printed using tabulate's "grid" format.
 '''
-import sys
 import csv
+import sys
 from tabulate import tabulate
-
 
 def main():
     if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    elif not sys.argv[1].endswith(".csv"):
+        sys.exit("Too few arguments")
+    if len(sys.argv) > 2:
+        sys.exit("Too many arguments")
+    if not sys.argv[1].endswith(".csv"):
         sys.exit("Not a CSV file")
-
     try:
-        with open(sys.argv[1]) as file:
-            reader = csv.reader(file)
-            table = list(reader)
-
-        print(tabulate(table[1:], headers=table[0], tablefmt="grid"))
-
+        print(atomic_function(sys.argv[1]))
     except FileNotFoundError:
-        sys.exit("File does not exist")
+        sys.exit("No such file")
 
-
+def atomic_function(filename):
+    with open(filename) as file:
+        table_data = []
+        reader = csv.reader(file)
+        for row in reader:
+            table_data.append(row)
+        formatted_result = tabulate(table_data, headers="firstrow", tablefmt="grid")
+        return formatted_result
 if __name__ == "__main__":
     main()
