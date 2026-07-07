d11a402f333f0fb710caed2ba8ecb0fe452a42ee
Jodrohan <rohansomriya12345@gmail.com>
2026-07-07 14:09:05 +0530

    feat(response): implement email validation using validators

--- cs50p-python/weeks/week-7-regular-expressions/assignments/response.py
+++ cs50p-python/weeks/week-7-regular-expressions/assignments/response.py
@@ -1,7 +1,7 @@
 """
 CS50P: Response Validation
 
-Implement a program that prompts the user for an email address and then prints 
+Implement a program that prompts the user for an email address and then prints
 "Valid" or "Invalid", respectively, if the input is a syntactically valid email address.
 
 Constraints:
@@ -9,3 +9,29 @@ Constraints:
 - You must use a third-party library from PyPI (e.g., `validator-collection` or `validators`).
 - Do not validate whether the email address's domain name actually exists, only its syntax.
 """
+
+import validators
+
+
+def main():
+
+    email = input("What's email? ")
+
+    if validator(email):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def validator(s):
+    s = s.strip().lower()
+    is_valid = validators.email(s)
+
+    if is_valid:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
