5932a016ae43c0599836b8d0d00f23a38fc3be1a
Jodrohan <rohansomriya12345@gmail.com>
2026-07-07 13:27:33 +0530

    style(um): apply black PEP 8 formatting to implementation and tests

--- cs50p-python/weeks/week-7-regular-expressions/assignments/test_um.py
+++ cs50p-python/weeks/week-7-regular-expressions/assignments/test_um.py
@@ -1,15 +1,24 @@
 from um import count
 import pytest
 
+
 def test_single_um():
     assert count("um") == 1
+
 
 def test_word_with_um_in_it():
     assert count("umbrella") == 0
     assert count("numbera") == 0
 
+
 def test_multiple():
     assert count("Hey i am um um is the um of the um") == 4
 
+
 def test_case_ins():
-    assert count("Um is one of the goatest of the all time in the Um history of um in the house of targaryans of the  Um") == 4
+    assert (
+        count(
+            "Um is one of the goatest of the all time in the Um history of um in the house of targaryans of the  Um"
+        )
+        == 4
+    )
--- cs50p-python/weeks/week-7-regular-expressions/assignments/um.py
+++ cs50p-python/weeks/week-7-regular-expressions/assignments/um.py
@@ -1,8 +1,8 @@
 """
 Regular, um, Expressions
 
-Implement a function called count that expects a line of text as input as a str 
-and returns, as an int, the number of times that "um" appears in that text, 
+Implement a function called count that expects a line of text as input as a str
+and returns, as an int, the number of times that "um" appears in that text,
 case-insensitively, as a word unto itself, not as a substring of some other word.
 
 For instance:
@@ -12,19 +12,21 @@ For instance:
 
 Structure:
 - um.py must contain the main function and the count function.
-- test_um.py must contain test functions (e.g., test_single_um, test_word_with_um_in_it) 
+- test_um.py must contain test functions (e.g., test_single_um, test_word_with_um_in_it)
   to rigorously test the count function using pytest.
 """
+
 import re
+
 
 def main():
     user_input = input("What's string? ")
     print(count(user_input))
 
+
 def count(s):
     return len(re.findall(r"\bum\b", s, re.IGNORECASE))
-    
+
+
 if __name__ == "__main__":
     main()
-
-
