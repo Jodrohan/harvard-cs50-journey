f3597caa0f03e845403977ce195ac89a95b3399e
Jodrohan <rohansomriya12345@gmail.com>
2026-07-07 11:29:22 +0530

    docs(um): add assignment docstring

---
+++ cs50p-python/weeks/week-7-regular-expressions/assignments/um.py
@@ -0,0 +1,17 @@
+"""
+Regular, um, Expressions
+
+Implement a function called count that expects a line of text as input as a str 
+and returns, as an int, the number of times that "um" appears in that text, 
+case-insensitively, as a word unto itself, not as a substring of some other word.
+
+For instance:
+- count("hello, um, world") should return 1
+- count("yummy") should return 0
+- count("UM, what are you doing?") should return 1
+
+Structure:
+- um.py must contain the main function and the count function.
+- test_um.py must contain test functions (e.g., test_single_um, test_word_with_um_in_it) 
+  to rigorously test the count function using pytest.
+"""
