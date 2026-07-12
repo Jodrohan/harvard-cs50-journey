620b01d96dabeecbdbabf529be7134c0e961438d
Jodrohan <rohansomriya12345@gmail.com>
2026-07-12 23:39:16 +0530

    feat(week8): implement Jar class

--- cs50p-python/weeks/week-8-object-oriented-programming/assignments/jar.py
+++ cs50p-python/weeks/week-8-object-oriented-programming/assignments/jar.py
@@ -18,3 +18,31 @@ Implement a class called Jar in a file called jar.py with the following methods 
 
 - @property size: Returns the number of cookies actually in the cookie jar.
 """
+
+class Jar:
+    def __init__(self, capacity=12):
+        if not isinstance(capacity, int) or capacity < 0:
+            raise ValueError
+        self._capacity = capacity
+        self._size = 0
+
+    def __str__(self):
+        return "🍪" * self.size
+
+    def deposit(self, n):
+        if self.size + n > self.capacity:
+            raise ValueError
+        self._size += n
+
+    def withdraw(self, n):
+        if n > self.size:
+            raise ValueError
+        self._size -= n
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def size(self):
+        return self._size
