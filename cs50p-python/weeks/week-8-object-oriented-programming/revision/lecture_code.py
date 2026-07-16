ae76aed224bcabd2409a5cba63375806d00741f2
Jodrohan <rohansomriya12345@gmail.com>
2026-07-16 23:49:50 +0530

    refactor(week-8): implement oop validation and representation in lecture_code.py

---
+++ cs50p-python/weeks/week-8-object-oriented-programming/revision/lecture_code.py
@@ -0,0 +1,44 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if not house:
+            raise ValueError("Missing house")
+        self._house = house
+
+
+def main():
+    student = Student.get()
+    if student.name == "anoop":
+        student.house = "bhosdipur"
+    print(student)
+
+
+if __name__ == "__main__":
+    main()
